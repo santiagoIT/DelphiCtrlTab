81904393d6561f013cb27b3829b909a5b3b8a07c
santiagoIT <sburbano@itworks.ec>
2021-09-10 10:36:34 -0500

    Support for Delphi 11

--- msbuild/buildAll.bat
+++ msbuild/buildAll.bat
@@ -12,6 +12,14 @@ SET MSB=C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild
 IF NOT EXIST "%MSB%" SET MSB=C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin
 IF NOT EXIST "%MSB%" SET MSB=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin
 
+REM Delphi 11 - Alexandria
+SET BDSVERSION=22.0
+SET BDS=C:\Program Files (x86)\Embarcadero\Studio\%BDSVERSION%
+IF NOT EXIST "%BDS%" SET BDS=C:\Program Files (x86)\Embarcadero\Studio\%BDSVERSION%
+
+SET BDSCOMMONDIR=C:\Users\Public\Documents\Embarcadero\Studio\%BDSVERSION%
+"%MSB%\MSBuild.exe" DelphiCtrlTab.proj /t:CompileD28 /p:OutDir=%batPath%\build\bin\
+
 REM Delphi Sydney
 SET BDSVERSION=21.0
 SET BDS=C:\Program Files (x86)\Embarcadero\Studio\%BDSVERSION%
